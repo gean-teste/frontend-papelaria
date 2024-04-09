3a9a3f4c99ef49f4728954b33c93c5861686367f
gean-teste <133615196+gean-teste@users.noreply.github.com>
2024-04-09 11:22:53 -0300

    atualizamos as rotas

--- src/pages/logon/index.js
+++ src/pages/logon/index.js
@@ -1,13 +1,23 @@
 import React from "react";
 import '../../global.css'
 import Logo from '../../assets/img/fundo.jpg'
+import {useNavigate} from 'react-router-dom'
+
 export default function Logon(){
+   const navigate = useNavigate();
+   const logar=(e)=>{
+        e.preventDefault()  //impede que a página se recarregue quando o botão for  clicado
+      //   alert("deu?")     //apresenta uma mensagem de teste, pode ser removida em produção
+        navigate('/dashboard')//redirecion a para a home após o login 
+        
+   }
+
  return(
     <div className="logon-container"> 
    <section className="form">
       <h1 className="ajuste-fonte">Papelaria</h1>
     <img src={Logo} width={350}/>
-       <form>
+       <form onSubmit={logar}>
         <input 
         placeholder="E-mail"
         type="email" />
